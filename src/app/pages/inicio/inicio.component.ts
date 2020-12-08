7757aefd7c53776da6c65062270aedc08876a6be
pstefy <p.stefani16@gmail.com>
2020-12-08 19:11:56 -0400

    Carrusel

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -12,4 +12,31 @@ export class InicioComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  cambiar_1(): void {
+    var imagen_3 = document.getElementById('img_3');
+    var imagen_2 = document.getElementById('img_2');
+    var imagen_1 = document.getElementById('img_1');
+    imagen_3.style.opacity = '0';
+    imagen_2.style.opacity = '0';
+    imagen_1.style.opacity = '1';
+  }
+
+  cambiar_2(): void {
+    var imagen_3 = document.getElementById('img_3');
+    var imagen_2 = document.getElementById('img_2');
+    var imagen_1 = document.getElementById('img_1');
+    imagen_3.style.opacity = '0';
+    imagen_2.style.opacity = '1';
+    imagen_1.style.opacity = '0';
+  }
+
+  cambiar_3(): void {
+    var imagen_3 = document.getElementById('img_3');
+    var imagen_2 = document.getElementById('img_2');
+    var imagen_1 = document.getElementById('img_1');
+    imagen_3.style.opacity = '1';
+    imagen_2.style.opacity = '0';
+    imagen_1.style.opacity = '0';
+  }
+
 }
